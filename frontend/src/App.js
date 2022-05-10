149b9a801a72f8b39ad7af0679d425fa499a0ebb
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-05-10 14:37:49 +0300

    frontend, инициализация

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>Веселье начинается</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/axios/axios.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+export const API_URL = `http://localhost:8000/api`
+
+const axiosInstance = axios.create({
+  withCredentials: true,
+  baseURL: API_URL
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+  return config
+})
+
+axiosInstance.interceptors.response.use((config) => {
+  return config;
+},async (error) => {
+  const originalRequest = error.config
+  if (error.response.status === 401 && error.config && !error.config._isRetry) {
+    originalRequest._isRetry = true;
+    try {
+      const response = await axios.get(`${API_URL}/auth/refresh`, {withCredentials: true})
+      localStorage.setItem('token', response.data.accessToken)
+      return axiosInstance.request(originalRequest);
+    } catch (e) {
+      console.log('НЕ АВТОРИЗОВАН')
+    }
+  }
+  throw error
+})
+
+export default axiosInstance
