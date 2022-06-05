90cb6a8bc34a3f2f320be33022fa32d9c7fb8484
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-06-05 18:42:51 +0300

    Тесты auth, кроме logout и части refresh

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -35,3 +35,127 @@ class TestAuth(BaseTestCase):
         )
 
         self.assertEqual(response.status_code, 201)
+        models.Tokens.parse_obj(response.json())
+        # TODO проверка в базе User, Profile, RefreshToken, проверка refreshToken в cookie
+
+    def test_user_already_exist_registration(self):
+        response = self.client.post(
+            f"{self.auth_url}/registration",
+            json={
+                "login": "user",
+                "name": "Админ",
+                "surname": "Админский",
+                "password": "123"
+            }
+        )
+
+        self.assertEqual(response.status_code, 409)
+
+    def test_bad_request_registration(self):
+        response = self.client.post(
+            f"{self.auth_url}/registration",
+            json={}
+        )
+
+        self.assertEqual(response.status_code, 422)
+
+    def test_success_login(self):
+        response = self.client.post(
+            f"{self.auth_url}/login",
+            data={
+                "username": "user",
+                "password": "password"
+            }
+        )
+
+        self.assertEqual(response.status_code, 200)
+        models.Tokens.parse_obj(response.json())
+        # TODO проверка в базе RefreshToken, проверка refreshToken в cookie
+
+    def test_wrong_user_login(self):
+        response = self.client.post(
+            f"{self.auth_url}/login",
+            data={
+                "username": "user2",
+                "password": "password"
+            }
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"detail": "Пользователь с таким логином не найден"})
+
+    def test_wrong_password_login(self):
+        response = self.client.post(
+            f"{self.auth_url}/login",
+            data={
+                "username": "user",
+                "password": "asd"
+            }
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"detail": "Неверный пароль"})
+
+    def test_success_refresh(self):
+        response = self.client.post(
+            f"{self.auth_url}/login",
+            data={
+                "username": "user",
+                "password": "password"
+            }
+        )
+
+        self.assertEqual(response.status_code, 200)
+        tokens = models.Tokens.parse_obj(response.json())
+
+        refresh_response = self.client.get(
+            f"{self.auth_url}/refresh",
+            cookies={REFRESH_TOKEN_COOKIE_KEY: tokens.refresh_token}
+        )
+
+        self.assertEqual(refresh_response.status_code, 200)
+        models.Tokens.parse_obj(refresh_response.json())
+        # TODO проверка в базе RefreshToken, проверка refreshToken в cookie
+
+    def test_refresh_without_cookie(self):
+        refresh_response = self.client.get(
+            f"{self.auth_url}/refresh"
+        )
+
+        self.assertEqual(refresh_response.status_code, 401)
+
+    def test_refresh_bad_cookie(self):
+        refresh_response = self.client.get(
+            f"{self.auth_url}/refresh",
+            cookies={REFRESH_TOKEN_COOKIE_KEY: "maybeRefresh<-_->"}
+        )
+
+        self.assertEqual(refresh_response.status_code, 401)
+        self.assertEqual(refresh_response.json(), {"detail": "Не валидный refresh_token"})
+
+    def test_refresh_bad_miss_refresh_token_in_bd(self):
+        response = self.client.post(
+            f"{self.auth_url}/login",
+            data={
+                "username": "user",
+                "password": "password"
+            }
+        )
+
+        self.assertEqual(response.status_code, 200)
+        tokens = models.Tokens.parse_obj(response.json())
+
+        # Имитация, что из базы пропал refresh_token
+        test_session = next(override_get_session())
+        test_session.query(tables.RefreshToken).delete()
+        test_session.commit()
+
+        refresh_response = self.client.get(
+            f"{self.auth_url}/refresh",
+            cookies={REFRESH_TOKEN_COOKIE_KEY: tokens.refresh_token}
+        )
+
+        self.assertEqual(refresh_response.status_code, 401)
+        self.assertEqual(refresh_response.json(), {"detail": "Не удалось обновить токены"})
+
+    # TODO тест logout
