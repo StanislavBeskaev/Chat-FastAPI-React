dab41f87c6da0257a14977fd3b5bc338d3f5c419
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-06-21 21:15:35 +0300

    fronted, заготовка под измение чата

--- frontend/src/components/Sidebar/Chats.js
+++ frontend/src/components/Sidebar/Chats.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { ListGroup } from 'react-bootstrap'
+import {Button, ListGroup} from 'react-bootstrap'
 import {observer} from 'mobx-react-lite'
 
 import messagesStore from '../../stores/messagesStore'
 import {useSocket} from '../../contexts/SocketProvider'
+import authStore from '../../stores/authStore'
 
 
 const Chats = () => {
   const {sendStopTyping} = useSocket()
   const {chats, selectedChatId, selectedChatTyping} = messagesStore
+  const {user} = authStore
 
   const changeChat = (chatId) => {
     if (selectedChatTyping) {
@@ -21,16 +23,34 @@ const Chats = () => {
 
   return (
     <ListGroup variant="flush">
-      {Object.keys(chats).map(chatId => (
+      {Object.keys(chats).map(chatId => {
+        const isOwner = user.login === chats[chatId].creator
+        const chatName = chats[chatId].chat_name
+        return (
         <ListGroup.Item
           key={chatId}
           action
           onClick={() => changeChat(chatId)}
           active={selectedChatId === chatId}
         >
-          {chats[chatId].chat_name}
-        </ListGroup.Item>
-      ))}
+          <div className="d-flex justify-content-between">
+            {chatName}
+            {
+              isOwner
+                ? <Button
+                  variant="success"
+                  size="sm"
+                  onClick={e => {
+                    e.stopPropagation()
+                    alert(`Тут будет изменение чата ${chatName}`)
+                  }
+                  }
+                >Изменить</Button>
+                : null
+            }
+          </div>
+        </ListGroup.Item>)
+      })}
     </ListGroup>
   )
 }
