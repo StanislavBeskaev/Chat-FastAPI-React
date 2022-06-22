2c591cccb559591a67aadb5bb91641509cf585fc
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-06-22 22:27:44 +0300

    fronted, Разный цвет карандаша редактирования чата

--- frontend/src/components/Sidebar/Chats.js
+++ frontend/src/components/Sidebar/Chats.js
@@ -2,7 +2,9 @@ import React from 'react'
 import {Image, ListGroup} from 'react-bootstrap'
 import {observer} from 'mobx-react-lite'
 
-import pencil from "../../img/pencil.ico"
+import white_pencil from "../../img/white_pencil.png"
+import blue_pencil from "../../img/blue_pencil.png"
+
 
 import messagesStore from '../../stores/messagesStore'
 import {useSocket} from '../../contexts/SocketProvider'
@@ -28,25 +30,28 @@ const Chats = () => {
       {Object.keys(chats).map(chatId => {
         const isOwner = user.login === chats[chatId].creator
         const chatName = chats[chatId].chat_name
+        const selected = selectedChatId === chatId
         return (
         <ListGroup.Item
           key={chatId}
           action
           onClick={() => changeChat(chatId)}
-          active={selectedChatId === chatId}
+          active={selected}
         >
           <div className="d-flex justify-content-between">
             {chatName}
             {
               isOwner
                 ? <Image
-                  src={pencil}
-                  height={20}
-                  onClick={e => {
-                    e.stopPropagation()
-                    changeChatNameModalStore.openWithChatId(chatId)
-                  }}
-                />
+                    src={selected ? white_pencil : blue_pencil}
+                    height={20}
+                    onMouseEnter={e => e.target.height = 23}
+                    onMouseLeave={e => e.target.height = 20}
+                    onClick={e => {
+                      e.stopPropagation()
+                      changeChatNameModalStore.openWithChatId(chatId)
+                    }}
+                  />
                 : null
             }
           </div>
