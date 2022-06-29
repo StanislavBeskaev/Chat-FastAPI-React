d8a93a484531c36d7ef34ec0c8916db4c5520940
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-06-29 22:44:24 +0300

    Иконки переключателей чата и контактов, цвет фона для переключателей

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
-import {Tab, Nav, Button, Row, Modal} from 'react-bootstrap'
+import {Tab, Nav, Button, Row, Modal, Image} from 'react-bootstrap'
 import {observer} from 'mobx-react-lite'
 
 import authStore from '../../stores/authStore'
@@ -14,9 +14,17 @@ import NewChatModal from '../Modals/NewChatModal'
 import newChatModalStore from '../../stores/modals/newChatModalStore'
 import UserAvatar from '../Avatars/UserAvatar'
 
+import chats_blue from '../../img/chats_blue.png'
+import chats_white from '../../img/chats_white.png'
+import contact_blue from '../../img/contact_blue.png'
+import contact_white from '../../img/contact_white.png'
+
 
 const CHATS_KEY = 'chats'
 const CONTACTS_KEY = 'contacts'
+const ICON_HEIGHT = 30
+const PASSIVE_BACKGROUND_COLOR = 'white'
+const ACTIVE_BACKGROUND_COLOR = 'dodgerblue'
 
 function Sidebar({ login }) {
   const [activeKey, setActiveKey] = useState(CHATS_KEY)
@@ -24,15 +32,33 @@ function Sidebar({ login }) {
   return (
     <div style={{maxWidth: '25%'}} className="d-flex flex-column flex-grow-1">
       <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
-        <Nav variant="tabs" className="justify-content-center">
-          <Nav.Item>
-            <Nav.Link eventKey={CHATS_KEY} className="btn">Чаты</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey={CONTACTS_KEY} className="btn">Контакты</Nav.Link>
-          </Nav.Item>
-        </Nav>
         <Tab.Content className="border-end overflow-auto flex-grow-1">
+          <Nav fill variant="tabs" className="justify-content-center">
+            <Nav.Item>
+              <Nav.Link
+                eventKey={CHATS_KEY}
+                className="btn"
+                style={{backgroundColor: activeKey === CHATS_KEY ? ACTIVE_BACKGROUND_COLOR : PASSIVE_BACKGROUND_COLOR}}
+              >
+                <Image
+                  src={activeKey === CHATS_KEY ? chats_white : chats_blue}
+                  height={ICON_HEIGHT}
+                />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey={CONTACTS_KEY}
+                className="btn"
+                style={{backgroundColor: activeKey === CONTACTS_KEY ? ACTIVE_BACKGROUND_COLOR : PASSIVE_BACKGROUND_COLOR}}
+              >
+                <Image
+                  src={activeKey === CONTACTS_KEY ? contact_white : contact_blue}
+                  height={ICON_HEIGHT}
+                />
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
           <Tab.Pane eventKey={CHATS_KEY}>
             <Chats/>
           </Tab.Pane>
